0ee7f0a3d8cde9cc35bb05e26ef24efb6a7afdec
Anton Palermo <anton.palermo+github@dragonpay.ph>
2025-05-30 21:34:36 +0800

    react-declerative done

--- apps/react-declerative/src/main.tsx
+++ apps/react-declerative/src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <h1>hello-world</h1>,
-);
+// createRoot(document.getElementById('root') as HTMLElement).render(
+//   <h1>hello-world</h1>,
+// );
+
+const heading = document.createElement('h1');
+heading.textContent = 'This is a heading content';
+heading.className = 'heading';
+
+const container = document.getElementById('root');
+
+container?.appendChild(heading);
