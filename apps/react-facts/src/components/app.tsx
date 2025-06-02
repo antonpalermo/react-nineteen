be485c63619dbf1eaabbb1310e1126fecf91c1a7
Anton Angelo N. Palermo <ANPBusinessmail@gmail.com>
2025-06-02 22:38:19 +0800

    react-facts: initial project setup

--- apps/react-facts/src/components/app.tsx
+++ apps/react-facts/src/components/app.tsx
@@ -1,3 +1,11 @@
+import Main from './main';
+import Navbar from './navbar';
+
 export default function App() {
-  return <h1>App</h1>;
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  );
 }
