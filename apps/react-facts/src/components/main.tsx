ccfc1843cd2c4673fb31a14ca8acc2bfa49b124f
Anton Angelo N. Palermo <ANPBusinessmail@gmail.com>
2025-06-03 22:27:22 +0800

    refac: (react-facts) update main content heading

--- apps/react-facts/src/components/main.tsx
+++ apps/react-facts/src/components/main.tsx
@@ -1,7 +1,7 @@
 export default function Main() {
   return (
     <main>
-      <h1>React Facts</h1>
+      <h1>Fun facts about React!</h1>
       <ul>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
