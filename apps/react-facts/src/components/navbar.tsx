aac362a8a98a037c7120b29fcbabde485b82e0a5
Anton Angelo N. Palermo <ANPBusinessmail@gmail.com>
2025-06-03 22:15:00 +0800

    react-facts: styling navbar

--- apps/react-facts/src/components/navbar.tsx
+++ apps/react-facts/src/components/navbar.tsx
@@ -1,3 +1,12 @@
+import Logo from '../assets/react_logo.png';
+
 export default function Navbar() {
-  return <h1>Navbar goes here.</h1>;
+  return (
+    <header>
+      <nav>
+        <img src={Logo} alt="react logo" />
+        <span>React Facts</span>
+      </nav>
+    </header>
+  );
 }
