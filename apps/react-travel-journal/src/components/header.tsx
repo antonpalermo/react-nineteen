73d8af30d60a08d30c6ac78a23b0565b5fdec749
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-06-07 23:38:03 +0800

    wip(travel-journal): initialize tailwind

--- apps/react-travel-journal/src/components/header.tsx
+++ apps/react-travel-journal/src/components/header.tsx
@@ -2,9 +2,9 @@ import Globe from '../assets/globe.svg';
 
 export default function Header() {
   return (
-    <header>
-      <img src={Globe} alt="globe icon" />
-      <h1>My Travel Journal</h1>
+    <header className="bg-[#f55a5a] text-white w-full inline-flex items-center justify-center min-h-16">
+      <img src={Globe} alt="globe icon" className="w-6 mr-2" />
+      <h1 className="font-semibold">My Travel Journal</h1>
     </header>
   );
 }
