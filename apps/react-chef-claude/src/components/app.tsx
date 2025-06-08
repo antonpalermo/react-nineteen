a0ab3b66a6d28c1a64eaa2ed53ffe7c17b6cd56e
Anton Angelo N. Palermo <ANPBusinessmail@gmail.com>
2025-06-08 22:54:19 +0800

    chef-claud: heading

--- apps/react-chef-claude/src/components/app.tsx
+++ apps/react-chef-claude/src/components/app.tsx
@@ -1,3 +1,9 @@
+import Heading from './heading';
+
 export default function App() {
-  return <h1 className="text-9xl text-green-500">Chef Claude</h1>;
+  return (
+    <>
+      <Heading />
+    </>
+  );
 }
---
+++ apps/react-chef-claude/src/components/heading.tsx
@@ -0,0 +1,11 @@
+export default function Heading() {
+  return (
+    <header className="border-b border-gray-200">
+      <nav className="max-w-5xl mx-auto px-5">
+        <div className="w-full py-5 inline-flex items-center justify-center">
+          <h1>Chef Claude</h1>
+        </div>
+      </nav>
+    </header>
+  );
+}
