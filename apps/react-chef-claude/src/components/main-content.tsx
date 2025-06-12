3ccbce5785e249b43302a93ff6d3610247c4a21e
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-06-12 23:07:19 +0800

    chef-claude: render ingredient array state

--- apps/react-chef-claude/src/components/main-content.tsx
+++ apps/react-chef-claude/src/components/main-content.tsx
@@ -1,14 +1,50 @@
-import { useState } from 'react';
-
-import IngredientForm from './ingredient-form';
+import { useState, useRef, FormEvent } from 'react';
 
 export default function MainContent() {
-  const [msg, setMsg] = useState('sample');
+  const [ingredients, setIngredients] = useState<string[]>([]);
+
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (formRef.current) {
+      const formData = new FormData(formRef.current);
+
+      const newIngredient = formData.get('ingredient') as string;
+      setIngredients(prev => [...prev, newIngredient]);
+
+      formRef.current.reset();
+    }
+  }
+
+  const Ingredients = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
 
   return (
     <main className="max-w-4xl mx-auto px-5">
       <div className="py-10">
-        <IngredientForm />
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="w-full sm:space-x-3 space-x-5 inline-flex items-center"
+        >
+          <input
+            type="text"
+            aria-label="add ingredient"
+            placeholder="Potatoes"
+            className="flex-1 border border-gray-200 px-3 py-2 rounded-md text-sm"
+            name="ingredient"
+          />
+          <button
+            type="submit"
+            className="p-2 bg-gray-900 px-3 text-white font-medium rounded-md text-sm"
+          >
+            Add Ingredient
+          </button>
+        </form>
+        {Ingredients}
       </div>
     </main>
   );
