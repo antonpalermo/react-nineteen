fd7e591514b4699f2e2e2dc4f373a2e71de3c825
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-06-13 23:27:35 +0800

    react-forms: setup basic form

--- apps/react-forms/src/components/app.tsx
+++ apps/react-forms/src/components/app.tsx
@@ -1,3 +1,22 @@
 export default function App() {
-  return <h1>App</h1>
+  function submitForm(formData: FormData) {
+    
+  }
+
+  return (
+    <form action={submitForm}>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" defaultValue={'Jane Doe'} />
+      <br />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        defaultValue={'Not Secure Password'}
+      />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
