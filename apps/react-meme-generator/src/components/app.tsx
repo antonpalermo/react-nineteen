2421e5f7802f86ebc9a1997db94d2df7e3611a52
Anton Angelo N. Palermo <ANPBusinessmail@gmail.com>
2025-06-23 23:11:26 +0800

    meme-generator: update heading

--- apps/react-meme-generator/src/components/app.tsx
+++ apps/react-meme-generator/src/components/app.tsx
@@ -1,3 +1,12 @@
+import Header from './header';
+
 export default function App() {
-  return <h1>Sample</h1>
+  return (
+    <>
+      <Header />
+      <div className="max-w-5xl sm:max-w-4xl mx-auto px-5">
+        <h1>Sample</h1>
+      </div>
+    </>
+  );
 }
---
+++ apps/react-meme-generator/src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="max-w-5xl mx-auto px-5">
+        <div className="py-5">
+          <h1 className="font-sans text-2xl font-medium">Meme Generator</h1>
+        </div>
+      </nav>
+    </header>
+  );
+}
