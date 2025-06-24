ee7d2394e6971845c92c2662f23c0ee0dde73fcb
Anton Angelo N. Palermo <ANPBusinessmail@gmail.com>
2025-06-24 23:02:45 +0800

    meme-generator: fetch meme images

--- apps/react-meme-generator/src/components/app.tsx
+++ apps/react-meme-generator/src/components/app.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import Header from './header';
 
 export default function App() {
+  const [imgs, setImgs] = useState();
+
+  useEffect(() => {
+    async function fetchImage() {
+      const response = await fetch('https://api.imgflip.com/get_memes');
+      setImgs(await response.json());
+    }
+
+    fetchImage();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="max-w-5xl sm:max-w-4xl mx-auto px-5">
-        <h1>Sample</h1>
+        {JSON.stringify(imgs)}
       </div>
     </>
   );
--- apps/react-meme-generator/src/components/header.tsx
+++ apps/react-meme-generator/src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
       <nav className="max-w-5xl mx-auto px-5">
-        <div className="py-5">
+        <div className="w-full py-5 inline-flex items-center justify-between">
           <h1 className="font-sans text-2xl font-medium">Meme Generator</h1>
         </div>
       </nav>
